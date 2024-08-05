1d62e91cdf241832249510d64f2383e8b48a764d
Nassabiq <36199713+Nassabiq@users.noreply.github.com>
2024-08-05 17:36:26 +0700

    Initial commit

---
+++ database/migrations/2024_08_02_083660_create_pernyataan_kuesioner_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pernyataan_kuesioner', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('variable_id');
+            $table->foreign('variable_id')->references('id')->on('variable_kuesioner')->onDelete('cascade');
+            
+            $table->string('indikator');
+            $table->string('kode_indikator');
+            $table->longText('isi_kuesioner');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pernyataan_kuesioner');
+    }
+};
---
+++ resources/views/app.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    
+    <section class="bg-gray-50">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+                <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+                    alt="logo">
+                Login
+            </a>
+            <div
+                class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    {{-- <h1
+                        class="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                        Login
+                    </h1> --}}
+                    <form class="space-y-4 md:space-y-6" action="#">
+                        <div>
+                            <label for="nim"
+                                class="block mb-2 text-sm font-medium text-gray-900">NIM</label>
+                            <input type="text" name="nim" id="nim"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                                placeholder="NIM..." required="">
+                        </div>
+                        <div>
+                            <label for="password"
+                                class="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                                required="">
+                        </div>
+                        <button type="submit"
+                            class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
